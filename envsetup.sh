05ec982ccb73fecd50b4a41e77878644ef33e53e
Chirayu Desai <cdesai@cyanogenmod.org>
2013-03-19 17:50:37 +0530

    envsetup: export ANDROID_BUILD_TOP earlier
    Change-Id: Ia3497f61f4e9aaeeb793cecd3b4f488f0a48181a

--- envsetup.sh
+++ envsetup.sh
@@ -324,8 +324,6 @@ function set_stuff_for_environment()
 {
     set_lunch_paths
     set_sequence_number
-
-    export ANDROID_BUILD_TOP=$(gettop)
 }
 
 function set_sequence_number()
@@ -1184,4 +1182,4 @@ set_global_paths
 source_vendorsetup
 addcompletions
 
-
+export ANDROID_BUILD_TOP=$(gettop)
